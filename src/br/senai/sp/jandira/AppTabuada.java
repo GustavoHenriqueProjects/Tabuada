9828d94b8fcefa173480055d6ab2a2007c43b772
Gustavo Henrique <Gustavo326silva@gmail.com>
2022-09-06 16:56:57 -0300

    Acresentado metodo Calculo

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,10 +1,19 @@
 package br.senai.sp.jandira;
 
+import br.senai.sp.jandira.model.ClienteTabuada;
+
 public class AppTabuada {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+	ClienteTabuada tabuada = new ClienteTabuada();
+	tabuada.multiplicando = 2;
+	tabuada.minimoMultiplicador = 17;
+	tabuada.maximoMultiplicador= 20;
+	
+	String[] resultado = tabuada.getTabuada();
+	
+	System.out.println(resultado[0]);
+ 	
+	
+	}	
 }
--- src/br/senai/sp/jandira/model/ClienteTabuada.java
+++ src/br/senai/sp/jandira/model/ClienteTabuada.java
@@ -1,5 +1,40 @@
 package br.senai.sp.jandira.model;
 
-public class ClienteTabuada {
+import java.util.ArrayList;
 
-}
+
+public class ClienteTabuada {
+	
+	//Atributos
+    public int multiplicando;
+    public int minimoMultiplicador;
+    public int maximoMultiplicador;
+    
+    public 	String[] getTabuada() {
+    	
+    	int tamanho = maximoMultiplicador-minimoMultiplicador + 1;
+    	String tabuada[] = new String[tamanho];
+    	
+    	int i =0;
+    	
+    	while (minimoMultiplicador <= maximoMultiplicador) {
+    		
+    		int produto = multiplicando*minimoMultiplicador;
+    		
+    		String resultado = multiplicando +"X"+minimoMultiplicador+"="+produto;
+    		tabuada[i] = resultado;
+    		
+    		i++;
+    		minimoMultiplicador++;
+    	}
+    	
+    	return tabuada;
+    		
+     }
+    
+ }
+	
+
+    	
+
+
